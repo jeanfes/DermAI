ace91fb02725be03580b76833e7f156f538e1a20
jeanfes <jeanescobar7@hotmail.com>
2023-05-30 16:17:40 -0500

    commit de prueba

--- src/components/chatbot/chatbot.jsx
+++ src/components/chatbot/chatbot.jsx
@@ -1,68 +1,88 @@
 import { useState, useEffect, useRef } from "react";
 import "./chatbot.css";
 import Send from "../../assets/send.png";
-export function Chatbot() {
-    const [messages, setMessages] = useState([]);
-    const [inputValue, setInputValue] = useState('');
-    const chatMessagesRef = useRef(null);
-    const handleMessageSubmit = (e) => {
-        e.preventDefault();
-        if (inputValue.trim() !== '') {
-            const newMessage = {
-                text: inputValue,
-                isUserMessage: true
-            };
-            setMessages([...messages, newMessage]);
-            setInputValue('');
-        }
-    };
-    useEffect(() => {
-        const scrollContainer = chatMessagesRef.current;
-        const startTime = performance.now();
-        const duration = 500;
-        const startOffset = scrollContainer.scrollTop;
-        const endOffset = scrollContainer.scrollHeight - scrollContainer.clientHeight;
-        let progress = 0;
-        const scrollStep = () => {
-            const currentTime = performance.now();
-            const elapsed = currentTime - startTime;
-            if (elapsed > 0) {
-                progress = elapsed / duration;
-                if (progress < 1) {
-                    const ease = easeOutCubic(progress);
-                    const newOffset = startOffset + (endOffset - startOffset) * ease;
-                    scrollContainer.scrollTop = newOffset;
-                    requestAnimationFrame(scrollStep);
-                } else {
-                    scrollContainer.scrollTop = endOffset;
-                }
-            } else {
-                requestAnimationFrame(scrollStep);
-            }
-        };
-        const easeOutCubic = (t) => {
-            return 1 - Math.pow(1 - t, 3);
-        };
 
-        requestAnimationFrame(scrollStep);
-    }, [messages]);
-    return (
-        <section className="section_chatbot">
-            <div className="chat-container">
-                <div ref={chatMessagesRef} className="chat-messages">
-                    {messages.map((message, index) => (
-                        <div key={index} className={`message ${message.isUserMessage ? 'user-message' : 'system-message'}`}>
-                            {message.text}
-                        </div>
-                    ))}
-                </div>
-                <form className="chat-input" onSubmit={handleMessageSubmit}>
-                    <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Escribe tu pregunta..." />
-                    <button type="submit">
-                        <img src={Send} alt="" />
-                    </button>
-                </form>
+export function Chatbot() {
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [disabled, setDisabled] = useState(false);
+  const [result, setResult] = useState('');
+  const chatMessagesRef = useRef(null);
+
+  const handleMessageSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim() !== '') {
+      const newUserMessage = {
+        text: inputValue,
+        isUserMessage: true
+      };
+      setMessages(prevMessages => [...prevMessages, newUserMessage]);
+      setInputValue("");
+    }
+  };
+
+  useEffect(() => {
+    const scrollContainer = chatMessagesRef.current;
+    scrollContainer.scrollTop = scrollContainer.scrollHeight;
+  }, [messages]);
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      setDisabled(true);
+      const response = await fetch("http://localhost:3000/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: inputValue }),
+      });
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+      setResult(data.result);
+      setInputValue("");
+      const newSystemMessage = {
+        text: data.result,
+        isUserMessage: false,
+      };
+      setMessages(prevMessages => [...prevMessages, newSystemMessage]);
+      setDisabled(false);
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  const handleCombinedSubmit = (event) => {
+    event.preventDefault();
+    handleMessageSubmit(event);
+    onSubmit(event);
+  };
+
+  return (
+    <section className="section_chatbot">
+      <div className="chat-container">
+        <div ref={chatMessagesRef} className="chat-messages">
+          {messages.map((message, index) => (
+            <div key={index} className={`message ${message.isUserMessage ? 'user-message' : 'system-message'}`}>
+              {message.text}
             </div>
-        </section>
-    );
+          ))}
+        </div>
+        <form className="chat-input" onSubmit={handleCombinedSubmit}>
+          <input disabled={disabled} type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Escribe tu pregunta..." />
+          {disabled == true && <div class="loader">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>}
+          {disabled == false && <button type="submit">
+            <img src={Send} alt="" />
+          </button>}
+        </form>
+      </div>
+    </section>
+  );
 }
--- src/components/facial-ia/facial-ia.jsx
+++ src/components/facial-ia/facial-ia.jsx
@@ -1,7 +1,19 @@
 import "./facial-ia.css";
+import React from "react";
+import Webcam from 'react-webcam';
+export function FacialIA() {
+    const webcamRef = React.useRef(null);
 
-export function FacialIA(params) {
-    return(
-        <section>Reconocimiento facial</section>
+    const capture = React.useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        console.log(imageSrc);
+    }, [webcamRef]);
+    return (
+        <section className="section_facil-ia">
+            <div>
+                <Webcam audio={false} ref={webcamRef} />
+                <button onClick={capture}>Capturar</button>
+            </div>
+        </section>
     );
 }
